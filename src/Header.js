c3298b36a22a53b179240758a1b5db999d1e69cf
George Nikoglou <georgenison@gmail.com>
2020-09-17 20:29:16 +0300

    First Day new logo

--- src/Header.js
+++ src/Header.js
@@ -19,7 +19,7 @@ function Header() {
             <div className = "header__left">
                 <MenuIcon/>
                     <img className = "header__logo" 
-                    src ="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/110px-YouTube_Logo_2017.svg.png" 
+                    src ="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1200px-Logo_of_YouTube_%282015-2017%29.svg.png" 
                     alt ="">
                     </img>
             </div>
